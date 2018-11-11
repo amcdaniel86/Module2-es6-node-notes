a9532731e6791a90f5f31de0a5367d4f48ac2724
Alex McDaniel <amcdaniel186@gmail.com>
2018-11-10 21:41:01 -0500

    update1

---
+++ app.js
@@ -0,0 +1,64 @@
+// function hello() {
+//   var name = "ironhacker";
+//   console.log("hello " + name);
+// }
+
+// hello();
+// console.log(name);
+
+// for (var i = 1; i <= 30; i++) {
+//   console.log("Iteration number: " + i);
+// }
+// console.log("After the loop", i);
+
+
+// for (let i = 1; i <= 30; i++) {
+//   console.log("Iteration number: " + i);
+// }
+// console.log("After the loop", i);
+
+//ADVANTAGE OF USING LET
+// let name = "ironhacker";
+
+// if(true) {
+//   let name = "ted";
+//   console.log("name inside if statement: " + name);
+// }
+
+// console.log("name outside if statement: " + name);
+
+// var name = "ironhacker";
+// if (true) {
+//   var name = "ted";
+//   console.log("name inside if statement: " + name);
+// }
+// console.log("name outside if statement: " + name);
+
+
+// console.log(Math.PI);
+
+// WRONG WAY TO USE A SET NUMBER
+// let apple = {
+//   color: 'red',
+//   price: 5
+// }
+
+// function finalPrice(fruit){
+//     return fruit.price + (fruit.price * 0.7);
+// }
+
+// CORRECT WAY IS TO USE CONST
+// const TAX_RATE = .08;
+
+// let fruit = {
+  // ['apple', 'red', 5]
+  // ['orange', 'yellow', 4]
+// }
+
+// function finalPrice(f) {
+    // return f.[2] + (f.[2] * TAX_RATE);
+// }
+// console.log("Here's the total cost for a fruit: " + finalPrice(0))
+
+// not sure what to do with above
+
